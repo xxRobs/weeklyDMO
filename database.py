e53e567abdd2f3735beba8c3102a547083f6e94f
xxRobs <henriquedantas_2905@outlook.com>
2025-09-22 20:57:27 -0300

    Versão 2.2 (Att. Conexão BD)

--- database.py
+++ database.py
@@ -1,16 +1,14 @@
-from sqlmodel import SQLModel, create_engine
 import os
+from sqlmodel import SQLModel, create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não definida!")
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=True,
-    connect_args={
-        "sslmode": "require",            # ← Essencial para Supabase
-        "client_encoding": "utf8"        # ← Correção do seu erro!
-    }
-)
+engine = create_engine(DATABASE_URL, echo=True)
 
 def criar_banco():
     SQLModel.metadata.create_all(engine)
